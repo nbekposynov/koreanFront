34fdcf557589d496c733af94827ec8a35b9b6ca0
nbekposynov <nbekposynov@gmail.com>
2024-02-24 18:23:07 +0600

    kek

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,10 +1,16 @@
 <template>
-  <YandexMap :settings="settings" @click="openMarker = 0" class="map">
+  <YandexMap :settings="settings" class="map">
     <YandexMapDefaultSchemeLayer />
     <YandexMapDefaultFeaturesLayer />
     <YandexMapControls :settings="{ position: 'right' }" />
-    <YandexMapMarker :settings="{ coordinates:  [props.coords[1], props.coords[0]] }">
-      <div class="map-popup" @click="modal = !modal">
+    <!-- Итерация по всем контактам для создания маркеров -->
+    <YandexMapMarker
+      v-for="(contact, index) in contacts"
+      :key="contact.id"
+      :settings="{ coordinates: [contact.longitude, contact.latitude] }"
+      @click="toggleModal(index)"
+    >
+      <div class="map-popup">
         <img
           src="https://www.komatsu.ru/distributors/marker1.png"
           :style="{
@@ -15,11 +21,11 @@
             cursor: 'pointer',
           }"
         />
-        <div v-if="modal" class="map-popup__content">
-          <p>{{props.name}} </p>
-          <p>{{ props.address }}</p>
-          <p>{{props.phone}}</p>    
-          </div>   
+        <div v-if="activeMarkerId === index" class="map-popup__content">
+          <p>{{ contact.name }}</p>
+          <p>{{ contact.address }}</p>
+          <p>{{ contact.phone_number }}</p>
+        </div>
       </div>
     </YandexMapMarker>
   </YandexMap>
@@ -32,25 +38,40 @@ import {
   YandexMapDefaultFeaturesLayer,
   YandexMapDefaultSchemeLayer,
   YandexMapMarker,
-} from 'vue-yandex-maps';
-import { ref, defineProps } from 'vue';
+} from "vue-yandex-maps";
+import { watchEffect, ref, defineProps } from "vue";
 
 const props = defineProps({
-  coords: {
-    type: Array,
-    required: true
-  },
-  address: String ,
-  name: String,
-  phone: String
+  contacts: Array,
 });
+
 const settings = ref({
   location: {
-    center: [props.coords[1], props.coords[0]],
-    zoom: 17,
+    center: props.contacts.length
+      ? [props.contacts[0].longitude, props.contacts[0].latitude]
+      : [55.751244, 37.618423], // Москва, если контакты пусты
+    zoom: 12,
   },
 });
-const modal = ref(false);
+
+// Наблюдение за изменениями в contacts и обновление настроек карты
+watchEffect(() => {
+  if (props.contacts.length) {
+    settings.value.location.center = [
+      props.contacts[0].longitude,
+      props.contacts[0].latitude,
+    ];
+  }
+});
+const activeMarkerId = ref(null);
+
+const toggleModal = (index) => {
+  if (activeMarkerId.value === index) {
+    activeMarkerId.value = null;
+  } else {
+    activeMarkerId.value = index;
+  }
+};
 </script>
 
 <style>
@@ -66,7 +87,7 @@ const modal = ref(false);
   background-color: white; /* Background color for the text */
   padding: 5px;
   border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0,0,0,0.5);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
   font-size: 14px;
   justify-content: center;
 }
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -4,42 +4,68 @@ import IconLocation from "@/components/icon/location.vue";
 import IconPhone from "@/components/icon/phone.vue";
 import IconEmail from "@/components/icon/email.vue";
 import Map from "@/components/Map.vue";
-import {onMounted, ref, watch} from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useHttp } from "@/composables/useHttp.js";
-const { getCities, getBranch } = useHttp()
+const { getCities, getBranch } = useHttp();
 
-const cities = ref([])
-const selectedCityId = ref('')
-const contacts = ref([])
+const cities = ref([]);
+const selectedCityId = ref("");
+const contacts = ref([]);
 
 onMounted(async () => {
-  cities.value = await getCities()
-  selectedCityId.value = cities.value[0].id
-})
+  cities.value = await getCities();
+  selectedCityId.value = cities.value[0].id;
+});
 
 watch(selectedCityId, async (val) => {
-  contacts.value = await getBranch(val)
-})
+  contacts.value = await getBranch(val);
+});
 </script>
 
 <template>
-  <section class="lg:pt-24 md:pt-16 pt-8 flex flex-col lg:gap-20 md:gap-14 gap-8">
-    <h1 class="title container">Контакты и Региональные подразделения Ассоциации Корейцев Казахстана</h1>
-    <filters-list v-if="cities.length" :filters="cities" :field="'name'" @filter="selectedCityId = $event.id" class="px-calc"/>
-    <div v-for="item in contacts" :key="item.id" class="container flex flex-col items-center gap-9">
-      <Map :coords="[item.latitude, item.longitude]" :address="item.address" :name="item.name" :phone="item.phone_number" class="w-full"/>
-      <div class="flex flex-col items-center gap-3.5 lg:text-2xl md:text-xl text-base">
+  <section
+    class="lg:pt-24 md:pt-16 pt-8 flex flex-col lg:gap-20 md:gap-14 gap-8"
+  >
+    <h1 class="title container">
+      Контакты и Региональные подразделения Ассоциации Корейцев Казахстана
+    </h1>
+    <filters-list
+      v-if="cities.length"
+      :filters="cities"
+      :field="'name'"
+      @filter="selectedCityId = $event.id"
+      class="px-calc container flex flex1 "
+    />
+    <Map :contacts="contacts" class="w-full container" />
+    <div
+      v-for="item in contacts"
+      :key="item.id"
+      class="container flex flex-col items-center gap-9"
+    >
+      <div
+        class="flex flex-col items-center gap-3.5 lg:text-2xl md:text-xl text-base"
+      >
         <p class="font-bold">{{ item.name }}</p>
         <div class="flex gap-3.5 items-center">
-          <icon-location/>
+          <icon-location />
           <p>{{ item.address }}</p>
         </div>
-        <div v-for="phone in item.phone_number.match(/\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}/g)" :key="phone" class="flex gap-3.5 items-center">
-          <icon-phone class="pointer-events-none"/>
+        <div
+          v-for="phone in item.phone_number.match(
+            /\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}/g
+          )"
+          :key="phone"
+          class="flex gap-3.5 items-center"
+        >
+          <icon-phone class="pointer-events-none" />
           <p class="pointer-events-none">{{ phone }}</p>
         </div>
-        <a :href="`mailto:${item.email}`" class="flex gap-3.5 items-center">
-          <icon-email class="pointer-events-none"/>
+        <a
+          v-if="item.email"
+          :href="`mailto:${item.email}`"
+          class="flex gap-3.5 items-center"
+        >
+          <icon-email class="pointer-events-none" />
           <p class="pointer-events-none">{{ item.email }}</p>
         </a>
       </div>
@@ -48,5 +74,13 @@ watch(selectedCityId, async (val) => {
 </template>
 
 <style scoped>
-
+.flex1 {
+  display: flex;
+  flex-wrap: wrap; /* Позволяет элементам переноситься на новую строку */
+  justify-content: center; /* Выравнивает элементы в начале контейнера */
+  align-items: center; /* Центрирует элементы по вертикали */
+  gap: 10px; /* Задает расстояние между элементами */
+  max-height: 100%; /* Максимальная высота контейнера, регулируйте в зависимости от размера ваших элементов */
+  overflow: hidden; /* Скрывает элементы, выходящие за пределы контейнера */
+}
 </style>
